a2ace306aed2b61d44c4eacf45cc353c6a4406d6
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2025-06-30 22:21:14 -0400

    Initial commit

---
+++ src/js/artists.js
@@ -0,0 +1,33 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  /* TODO */
+  {
+    artistId: "AID-1",
+    name: "Alan Walker",
+    urls: [
+      { url: "https://alanwalker.com/", name: "Website" },
+      { url: "https://www.instagram.com/alanwalkermusic/?hl=en", name: "Instagram" }
+    ]
+  },
+  {
+    artistId: "AID-3",
+    name: "Marshmello",
+    urls: [
+      { url: "https://www.instagram.com/marshmello/", name: "Instagram" },
+      { url: "https://www.youtube.com/@marshmello", name: "youtube" }
+    ]
+  },
+  {
+    artistId: "AID-2",
+    name: "Adele",
+    urls: [
+      { url: "https://www.instagram.com/adele/", name: "Instagram" },
+      { url: "https://www.youtube.com/channel/UCsRM0YB_dabtEPGPTKo-gcw", name: "youtube" }
+    ]
+  }
+];
